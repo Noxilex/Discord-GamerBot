11ca418d7b1f951f3a680ef9c6adaa087d6a2b6e
Benjamin D'Hoop <dhoop59@hotmail.fr>
2016-12-31 12:23:16 +0100

    Replace repeated calls with one hash

--- main.rb
+++ main.rb
@@ -35,16 +35,20 @@ bot.command(:loltest) do |event, user, option, region="euw"|
 		champ_name = champ_data['name']
 		format_champs[champ_id] = champ_name
 	end
-	result = ""
-	format_champs.each do |key,value|
-		result += "#{value}\n"
-	end	
-	result
 end
 
 bot.command(:lolstats, channels: ["#bot_testing", "#spam", "#general"]) do |event, user, option, region="euw"|
 	api_key = configatron.lolAPI
 
+	#List all champion ids with associated name in hash (format_champs[id] = name)
+	champs = JSON.parse(Net::HTTP.get_response(URI('https://global.api.pvp.net/api/lol/static-data/'+region+'/v1.2/champion?api_key='+api_key)).body)
+	format_champs = Hash.new
+	champs['data'].each do |champ|
+		champ_data = champ[1]
+		champ_id = champ_data['id']
+		champ_name = champ_data['name']
+		format_champs[champ_id] = champ_name
+	end
 	
 
 	uri = URI('https://euw.api.pvp.net/api/lol/'+region+'/v1.4/summoner/by-name/'+user+'?api_key='+api_key)
@@ -54,25 +58,22 @@ bot.command(:lolstats, channels: ["#bot_testing", "#spam", "#general"]) do |even
 	player_id = obj[user.downcase]['id']
 	puts player_id
 	season = '6'
-	uri2 = URI('https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/'+player_id.to_s+'/ranked?season=SEASON2016&api_key='+api_key)
+	uri2 = URI('https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/'+player_id.to_s+'/ranked?season=SEASON201'+season+'&api_key='+api_key)
 	res2 = Net::HTTP.get_response(uri2)
 	obj2 = JSON.parse(res2.body)
+
 	result = "#{event.user.mention}\n"
 	result += "```Player: #{user}\n"
-	#Most played champions
 
-	
-	obj2['champions'].each do |champion| 
-		uri_champ = URI('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/'+champion['id'].to_s+'?api_key='+api_key)
-		res_champ = Net::HTTP.get_response(uri_champ)
-		obj_champ = JSON.parse(res_champ.body)
+	#Most played champions
+	obj2['champions'].each do |champion|
 		stats = champion['stats']
 		kills = stats['totalChampionKills'].to_f
 		assists = stats['totalAssists'].to_f
 		deaths = stats['totalDeathsPerSession'].to_f
 		kda = ((kills+assists)/deaths)
 		kda_format = "%5.2f" % kda
-		result += "Champion: #{obj_champ['name']}\n";
+		result += "Champion: #{champs[champion['id']]}\n";
 		result += "\tKDA: #{kda_format}\n"
 	end
 	
